fa5b08ccc74c61c732e5c2eb21d46c5ed91f310c
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-01-06 17:29:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const friend3 = "Toni";
 const friends  = ["Paris", "Meghan", "Toni"];
 console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
-console.log(years);
+const y = new Array(1991, 1984, 2008, 2020);
+console.log(y);
 
 console.log(friends[0]);
 // returns Paris
@@ -18,3 +18,21 @@ console.log(friends[friends.length-1]);
 
 friends[2] = "Toa";
 console.log(friends);
+
+
+const jonas = ["Jonas", "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+
+// EXERCISE
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
